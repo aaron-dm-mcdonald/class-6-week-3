17ee93d20d90baf0977858dbcc4f2c51e3e6f1d1
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-10-01 23:48:55 -0700

    add tree script for README

--- repo/push.sh
+++ repo/push.sh
@@ -2,8 +2,8 @@
 
 
 # git init
-git add ./repo/push.sh
-git commit -m "update the update script"
+git add ./repo/push.sh ./repo/tree.py
+git commit -m "add tree script for README"
 # git branch -M main
 # git remote add origin https://github.com/aaron-dm-mcdonald/class-6-week-3.git
 git push
