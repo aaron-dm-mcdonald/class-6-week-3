82c0cbadce55ca8a340171fddca51ba36ce67390
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-10-01 23:45:37 -0700

    update the update script

--- repo/push.sh
+++ repo/push.sh
@@ -2,8 +2,8 @@
 
 
 # git init
-git add .
-git commit -m "update readme, add boto3/py script"
+git add ./repo/push.sh
+git commit -m "update the update script"
 # git branch -M main
 # git remote add origin https://github.com/aaron-dm-mcdonald/class-6-week-3.git
 git push
