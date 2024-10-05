41ee880dbd2d3bc9c38e85fed5cebdc8af75a834
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-10-04 19:22:06 -0700

    rename

--- repo/push.sh
+++ repo/push.sh
@@ -3,7 +3,7 @@
 
 # git init
 git add .
-git commit -m "reorganize a bit, add auth test"
+git commit -m "rename"
 # git branch -M main
 # git remote add origin https://github.com/aaron-dm-mcdonald/class-6-week-3.git
 git push
